935b64351832ddf6c6c0e902edb63ba3989a0773
Devan Looches <devan.looches@gmail.com>
2021-04-23 08:51:45 -0700

    Added support for most language comments

--- lua/better-comments/init.lua
+++ lua/better-comments/init.lua
@@ -2,11 +2,14 @@ HighlightTags = {}
 HighlightTags["!"] = '#A00000'
 local highlightNS = vim.api.nvim_create_namespace("bettercomments")
 function HighlightUpdate()
+   vim.cmd'let g:commentstring = &commentstring'
+    local commentstring = string.gsub(vim.g["commentstring"],'%%s',"")
+    print(commentstring)
     vim.api.nvim_buf_clear_namespace(0,highlightNS,0,-1)
     local lines = vim.api.nvim_buf_get_lines(0,0,-1,true)
     for i,line in pairs(lines) do
         for k,v in pairs(HighlightTags) do
-            if line:match("^--"..k) ~= nil then
+            if line:match("^"..commentstring..k) ~= nil then
                 vim.highlight.create("bettercomments:"..k,{guibg=v})
                 vim.api.nvim_buf_add_highlight(0,highlightNS,"bettercomments:" ..k,i-1,0,-1)
             end
